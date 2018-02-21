21d2cc7a50b7dadeaeff91dfdbb650373612fa25
Michael Alvin <michael.alvin@stonybrook.edu>
2018-02-20 23:49:37 -0500

    second commitL auth enabled

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
-
-
 var nameText = document.getElementById("username");
 var postText = document.getElementById("post");
 var button = document.getElementById("postbtn");
+var emailText = document.getElementById("email");
+var passwordText = document.getElementById("password");
+var button = document.getElementById("postbtn");
 var firebaseRef = firebase.database().ref();
-
 
 /** Function to add a post to database **/
 function submit() {
@@ -15,6 +15,72 @@ function submit() {
     postText.value = "";
 }
 
+function login() {
+    var email = emailText.value;
+    var pw = passwordText.value;
+
+    firebase.auth().signInWithEmailAndPassword(email, pw).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        alert(errorMessage);
+    });
+}
+
+function signup() {
+    var email = emailText.value;
+    var pw = passwordText.value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, pw).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        alert(errorMessage);
+    });
+}
+
+function logout() {
+    firebase.auth().signOut().then(function() {
+        console.log('Signed Out');
+    }, function(error) {
+        console.error('Sign Out Error', error);
+    });
+}
+
+/** Function to add a listener for user auth **/
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    alert("Welcome " + email);
+    // ...
+    // Begin listening for data only if user is signed in
+    startListening();
+    
+  } else {
+    // User is signed out.
+    // ...
+    alert("Goodbye " + email);
+
+    //Delete conversation once user is logged out
+    var node = document.getElementById("results");
+    while (node.hasChildNodes()) {
+        node.removeChild(node.lastChild);
+    }
+  }
+});
+
+// Begin listening for data only if user is signed in
+// startListening();
+    
 /** Function to add a data listener **/
 var startListening = function() {
   	firebaseRef.on('child_added', function(snapshot) {
@@ -33,7 +99,4 @@ var startListening = function() {
     msgElement.className = "msg";
     document.getElementById("results").appendChild(msgElement);
     });
-  }
-
-// Begin listening for data
-startListening();
+}
