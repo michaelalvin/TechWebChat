7d9c2135b3d6c150cd1ed4f2446b6a863b5e5a3b
Michael Alvin <michael.alvin@stonybrook.edu>
2018-02-28 13:14:04 -0500

    each user can only see his/her own messages (needs admin)

--- public/index.js
+++ public/index.js
@@ -6,13 +6,39 @@ var passwordText = document.getElementById("password");
 var button = document.getElementById("postbtn");
 var firebaseRef = firebase.database().ref();
 
+var emailaddress;
+
 /** Function to add a post to database **/
 function submit() {
 	var username = nameText.value;
 	var post = postText.value;
 
-	firebaseRef.push({username:username, text:post});
-    postText.value = "";
+    //var ref = firebaseRef.child("users/email");
+    //var newre2 = ref.push({username:username, text:post});
+
+    //Wait email address is ready then add to tree
+    waitForElement();
+}
+
+function waitForElement(){
+    if(typeof emailaddress !== "undefined"){
+        //variable exists, do what you want
+        //replace @ and . because Firebase won't create child with those characters
+        var directory = "users/" + emailaddress;
+        directory = directory.replace("@", "_");
+	    directory = directory.replace(".", "-"); //use replace all instead
+        alert(directory);
+
+        var username = nameText.value;
+        var post = postText.value;
+
+        var userref = firebaseRef.child(directory);
+    	var messageref = userref.push({username:username, text:post});
+
+    	postText.value = "";
+    } else{
+        setTimeout(waitForElement, 250);
+    }
 }
 
 function login() {
@@ -51,22 +77,33 @@ function logout() {
 
 /** Function to add a data listener **/
 var startListening = function() {
-    firebaseRef.on('child_added', function(snapshot) {
-    var msg = snapshot.val();
-      
-    var msgUsernameElement = document.createElement("b");
-    msgUsernameElement.textContent = msg.username;
-        
-    var msgTextElement = document.createElement("p");
-    msgTextElement.textContent = msg.text;
-  
-    var msgElement = document.createElement("div");
-    msgElement.appendChild(msgUsernameElement);
-    msgElement.appendChild(msgTextElement);
+	if(typeof emailaddress !== "undefined"){
+		var directory = "users/" + emailaddress;
+		directory = directory.replace("@", "_");
+	    directory = directory.replace(".", "-"); //use replace all instead
 
-    msgElement.className = "msg";
-    document.getElementById("results").appendChild(msgElement);
-    });
+		userFirebaseRef = firebaseRef.child(directory);
+
+		//Previous: firebaseRef.on('child_added', function(snapshot)
+	    userFirebaseRef.on('child_added', function(snapshot) {
+	    var msg = snapshot.val();
+	      
+	    var msgUsernameElement = document.createElement("b");
+	    msgUsernameElement.textContent = msg.username;
+	        
+	    var msgTextElement = document.createElement("p");
+	    msgTextElement.textContent = msg.text;
+	  
+	    var msgElement = document.createElement("div");
+	    msgElement.appendChild(msgUsernameElement);
+	    msgElement.appendChild(msgTextElement);
+
+	    msgElement.className = "msg";
+	    document.getElementById("results").appendChild(msgElement);
+	    });
+	} else{
+        setTimeout(waitForElement, 250);
+    }
 }
 
 /** Function to add a listener for user auth **/
@@ -80,6 +117,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     var isAnonymous = user.isAnonymous;
     var uid = user.uid;
     var providerData = user.providerData;
+    emailaddress = user.email;
     alert("Welcome " + email);
     // ...
     startListening();
